ea505999eb17724ae39b9eecf52390df0d47cadc
Ritesh5-dev <ritesh.j@cloudworkmates.com>
2025-05-28 08:26:37 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-yum install -y httpd php
-systemctl enable httpd
-systemctl start httpd
+
+# Exit immediately if a command fails
+set -e
+
+# Update package index
+apt update -y
+
+# Install required tools
+apt install -y software-properties-common
+
+# Add Ondřej Surý’s PPA for PHP
+add-apt-repository ppa:ondrej/php -y
+apt update -y
+
+# Install Apache and PHP 8.1
+apt install -y apache2 php8.1 libapache2-mod-php8.1 php8.1-cli php8.1-common
+
+# Enable and start Apache
+systemctl enable apache2
+systemctl start apache2
